1cc991671b8110b77410da4d2c6998eb2b183c82
Tyler Huynh <huynht9@uci.edu>
2025-03-15 23:06:45 +0000

    Implemented is_adjacent() method

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -22,6 +22,10 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     return true;
 }
 
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     vector<string> ladder;
     if (begin_word == end_word) {
@@ -65,7 +69,7 @@ void print_word_ladder(const vector<string>& ladder) {
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
-    // my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
     // my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
     // my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
     // my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
