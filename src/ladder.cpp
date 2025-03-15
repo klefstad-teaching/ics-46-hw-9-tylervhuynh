8b1d78984a73aa7e2059eaad359362e6210b9929
Tyler Huynh <huynht9@uci.edu>
2025-03-15 23:03:03 +0000

    Implemented edit_distance_within() method

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,7 +6,48 @@ void error(string word1, string word2, string msg) {
     cerr << msg << " " << word1 << ", " << word2 << endl;
 }
 
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int len1 = str1.size();
+    int len2 = str2.size();
+    if (abs(len1 - len2) > d) {return false;}
+    int i = 0, j = 0, diff_count = 0;
+    while (i < len1 && j < len2) {
+        if (str1[i] != str2[j]) {
+            if (++diff_count > d) {return false;}
+            if (len1 > len2) {i++;}
+            else if (len1 < len2) {j++;}
+            else {i++; j++;}
+        } else {i++; j++;}
+    }
+    return true;
+}
+
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    vector<string> ladder;
+    if (begin_word == end_word) {
+        error("Word ladder could not be created: Begin and end word are equal", begin_word, end_word);
+        return ladder;
+    }
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()) {
+        vector<string> current_ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = current_ladder.back();
+        for (string word : word_list) {
+            if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(word);
+                if (word == end_word) {return new_ladder;}
+                ladder_queue.push(new_ladder);
+                visited.insert(word);
+            }
+        }
+    }
+    error("No ladder found.", begin_word, end_word);
+    return ladder;
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
