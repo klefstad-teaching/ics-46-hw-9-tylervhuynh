f9f31c650c3c1b0402fcc213a01238efb96b3367
Tyler Huynh <huynht9@uci.edu>
2025-03-17 02:39:04 +0000

    Implemented print_path() method

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -10,5 +10,12 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 }
 
 void print_path(const vector<int>& v, int total) {
-    
+    if (v.empty()) {cout << "No path found." << endl; return;}
+    cout << v.front() << endl;
+    cout << "Total cost " << total << endl;
+    for (int node : v) {
+        cout << node << " ";
+    }
+    cout << endl;
+    cout << "Total cost " << total << endl;
 }
