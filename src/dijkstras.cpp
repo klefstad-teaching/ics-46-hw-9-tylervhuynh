2457fa2212223ff430a6079edad91f857246fa21
Tyler Huynh <huynht9@uci.edu>
2025-03-17 02:47:19 +0000

    Implemented extact_shortest_path() method

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -2,11 +2,19 @@
 
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-
+    
 }
 
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
-
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    if (distances[destination] == INF) {return path;}
+    stack<int> temp_stack;
+    for (int at = destination; at != -1; at = previous[at]) {temp_stack.push(at);}
+    while (!temp_stack.empty()) {
+        path.push_back(temp_stack.top());
+        temp_stack.pop();
+    }
+    return path;
 }
 
 void print_path(const vector<int>& v, int total) {
